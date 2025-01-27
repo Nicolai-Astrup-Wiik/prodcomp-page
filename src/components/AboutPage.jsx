be28581310ff2468db724cc577b1b19039e70034
Nicolai-Astrup-Wiik <143996469+Nicolai-Astrup-Wiik@users.noreply.github.com>
2025-01-27 14:57:05 +0100

    fix font

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import styles from '../styles/AboutPage.module.css';
+import React from 'react';
+import styles from '../styles/AboutPage.module.css'; // Import CSS module
+
 export const AboutPage = () => {
 	return (
 		<>
-			<p>
-				Hvis du lurer på hva vi driver med eller om du vil drive med oss, ta kontakt med <a href="mailto:ivan@pappenheimer.no">Ivan</a> eller dra innom Sørkedalsveien 89 og spør etter oss...
+			<p className={styles.paragraph}>
+				Hvis du lurer på hva vi driver med eller om du vil drive med oss, ta kontakt med{' '}
+				<a href="mailto:ivan@pappenheimer.no">Ivan</a> eller dra innom Sørkedalsveien 89 og spør etter oss...
 			</p>
 		</>
-
-	)
-}
+	);
+};
