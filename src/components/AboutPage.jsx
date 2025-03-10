7fdeb42cc15d898a840c32557c2740ceb2174930
Nicolai-Astrup-Wiik <143996469+Nicolai-Astrup-Wiik@users.noreply.github.com>
2025-03-10 13:59:56 +0100

    fix about page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -4,9 +4,14 @@ import styles from '../styles/AboutPage.module.css'; // Import CSS module
 export const AboutPage = () => {
 	return (
 		<>
-			<p className={styles.paragraph} style={{ whiteSpace: "pre-line" }}>
-				{/*{"\"Ich kenne meine Pappenheimer\" sa Wallenstein i Schillers drama – og nå vet hele Norge hva han mente. Velkommen til Pappenheimer – filmproduksjonsselskapet som vet hva du tenker før du tenker det selv!\n\nVi har gjenopplivet arven etter den legendariske generalfeltmarskalk Gottfried Heinrich von Pappenheim - vi er kanskje ikke like krigerske, men absolutt like kompromissløse på kvalitet, bare med litt færre sverd involvert.\n\n«Jeg kjenner mine pappenheimere\" - altså du vet hvor du har oss: Forvent det uventede. Forvent det briljante. Forvent at vi leverer filmer som får selv den mest stoiske tilskuer til å heve et øyebryn i begeistring.\n\nSå hvis du lurer på hva vi driver med, eller om du vil «drive» med oss (vi regner allerede med at du vil det – vi kjenner våre pappenheimere), ta kontakt med Ivan eller stikk innom hovedkvarteret vårt i Maridalsveien 89. Bare spør etter \"de der Pappenheimer-folka\" – vi lover at vi ikke biter. Med mindre du ber pent om det. Eventuelt kan du plinge en mail til "}*/}
-				<a href="mailto:ivan@pappenheimer.no">Ivan</a>
+			<p className={styles.paragraph} style={{ whiteSpace: "pre-line", lineHeight: "1.6" }}>
+				Pappenheimer ble etablert i 2025, og vårt hovedfokus er å lage sterke reklamefilmer med kompromissløs kvalitet.
+				Hvis du vil jobbe med oss, ta kontakt med produsent <a href="mailto:ivan@pappenheimer.no">Ivan Jamne </a>, eller dra innom oss i Maridalsveien 89 og spør etter Pappenheimerne.
+				<br></br>
+				<br></br>
+				<strong><em>Pappenheimer (substantiv): En som man kjenner godt og vet hvordan vil opptre; en person man vet hvor man har.
+					Fra tysk Pappenheimer, avledet av slektsnavnet Pappenheim (jf. suffikset -er), opprinnelig betegnelse for lojal og hardtarbeidende soldat under feltherre Gottfried Heinrich greve av Pappenheim (1594–1632), en legendarisk generalfeltmarskalk.
+				</em></strong>
 			</p>
 		</>
 	);
