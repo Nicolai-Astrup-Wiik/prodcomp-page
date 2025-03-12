0b57cb7354b54a50a1c816022f6dbfedad789162
Nicolai-Astrup-Wiik <143996469+Nicolai-Astrup-Wiik@users.noreply.github.com>
2025-03-12 11:08:40 +0100

    update font

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -4,14 +4,16 @@ import styles from '../styles/AboutPage.module.css'; // Import CSS module
 export const AboutPage = () => {
 	return (
 		<>
-			<p className={styles.paragraph} style={{ whiteSpace: "pre-line", lineHeight: "1.6" }}>
+			<p className={styles.paragraph}>
 				Pappenheimer ble etablert i 2025, og vårt hovedfokus er å lage sterke reklamefilmer med kompromissløs kvalitet.
 				Hvis du vil jobbe med oss, ta kontakt med produsent <a href="mailto:ivan@pappenheimer.no">Ivan Jamne </a>, eller dra innom oss i Maridalsveien 89 og spør etter Pappenheimerne.
 				<br></br>
 				<br></br>
-				<strong><em>Pappenheimer (substantiv): En som man kjenner godt og vet hvordan vil opptre; en person man vet hvor man har.
+				<p className={styles.paragraphItalic} >
+					<h3>Pappenheimer (substantiv):</h3>
+					En som man kjenner godt og vet hvordan vil opptre; en person man vet hvor man har.
 					Fra tysk Pappenheimer, avledet av slektsnavnet Pappenheim (jf. suffikset -er), opprinnelig betegnelse for lojal og hardtarbeidende soldat under feltherre Gottfried Heinrich greve av Pappenheim (1594–1632), en legendarisk generalfeltmarskalk.
-				</em></strong>
+				</p>
 			</p>
 		</>
 	);
