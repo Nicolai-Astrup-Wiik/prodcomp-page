a6d03868506e5a22267edad78d1167f61feb7bde
Nicolai-Astrup-Wiik <143996469+Nicolai-Astrup-Wiik@users.noreply.github.com>
2025-03-25 13:45:59 +0100

    fix om oss

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import styles from '../styles/AboutPage.module.css'; // Import CSS module
+import { Link } from 'react-router-dom';
 
 export const AboutPage = () => {
 	return (
 		<>
 			<p className={styles.paragraph}>
-				Pappenheimer ble etablert i 2025, og vårt hovedfokus er å lage sterke reklamefilmer med kompromissløs kvalitet.
-				Hvis du vil jobbe med oss, ta kontakt med produsent <a href="mailto:ivan@pappenheimer.no">Ivan Jamne </a>, eller dra innom oss i Maridalsveien 89 og spør etter Pappenheimerne.
+				Pappenheimer er et produksjonsselskap som bærer samme navn som tidenes mest standhaftige feltmarskalk,
+				og vi er her for å mørne lattermuskler over det ganske land – med kompromissløshet, fingerspitzgefühl og glimt i øyet.
+
+				Pappenheimerne som står klare for hyre er regissørene <Link className={styles.linkButton} to="/directors/gaute">Gaute Hesthagen</Link>, <Link className={styles.linkButton} to="/directors/sigve">Sigve Aspelund</Link> og regiduoen <Link className={styles.linkButton} ti="/directors/nico&links">Nico & Links</Link>.
+				Produsent og våpendrager er <Link className={styles.linkButton} to="/contact">Ivan Jamne </Link> – en mann viden kjent for å stå støtt i feltmarskalkstøvlene uansett om produksjonsprosessen preges av solskinnsbris eller tett kuleregn.
+
+				Hvorfor dukker Pappenheimer opp akkurat nå? Fordi humor er altfor alvorlig til å bli tatt lett på. Det norske markedet er overmodent for enda en solid dose kvalitetshumor.
+				Er du i akutt behov for noe morsomt? En historie som fester seg som en kule i ryggen? Noe helt sjukt som får folk til å dele, le og huske? Da vet du hvem du skal ringe.
+
+				En Pappenheimer kjemper som kjent for klienten til siste sendekopi.
+				<br />
+				<br />
+				Pappenheimerene holder til i Maridalsveien 89
+
 				<br></br>
 				<br></br>
 				<p className={styles.paragraphItalic} >
